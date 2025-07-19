69e849af99e5c27d96353d28b40a12fda2c3026f
gepron1x <georgijpronuk@gmail.com>
2025-07-19 13:52:33 +0300

    add more variants for NeatMap

--- decalium-std-java/src/main/java/ru/decalium/std/java/collecitons/NeatMap.java
+++ decalium-std-java/src/main/java/ru/decalium/std/java/collecitons/NeatMap.java
@@ -2,21 +2,42 @@ package ru.decalium.std.java.collecitons;
 
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.function.Function;
 
 public final class NeatMap {
 
-    public static <K, V> Map<K, V> createMap(Function<V, K> keyMapper, Iterable<V> values) {
-        Map<K, V> map = new HashMap<>();
+    public static <K, V> Map<K, V> createMap(Map<K, V> map, Function<V, K> keyMapper, Iterable<V> values) {
         for(V value : values) {
             map.put(keyMapper.apply(value), value);
         }
         return map;
     }
 
-    @SafeVarargs
-    public static <K, V> Map<K, V> createMap(Function<V, K> keyMapper, V... values) {
-        return createMap(keyMapper, Arrays.asList(values));
+    public static <K, V> Map<K, V> createHashMap(Function<V, K> keyMapper, Iterable<V> values) {
+        return createMap(new HashMap<>(), keyMapper, values);
     }
+
+    public static <K, V> Map<K, V> createLinkedHashMap(Function<V, K> keyMapper, Iterable<V> values) {
+        return createMap(new LinkedHashMap<>(), keyMapper, values);
+    }
+
+    @SafeVarargs
+    public static <K, V> Map<K, V> createMap(Map<K, V> map, Function<V, K> keyMapper, V... values) {
+        return createMap(map, keyMapper, Arrays.asList(values));
+    }
+
+    @SafeVarargs
+    public static <K, V> Map<K, V> createHashMap(Function<V, K> keyMapper, V... values) {
+        return createMap(new HashMap<>(), keyMapper, values);
+    }
+
+    @SafeVarargs
+    public static <K, V> Map<K, V> createLinkedHashMap(Function<V, K> keyMapper, V... values) {
+        return createMap(new LinkedHashMap<>(), keyMapper, values);
+    }
+
+
+
 }
